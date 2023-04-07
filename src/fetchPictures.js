71bc56b8c9b1549cc18c40a8300b17484dab5849
OIgaB <linkbolya@gmail.com>
2023-04-07 15:03:52 +0300

    add async await & axios library

--- src/fetchPictures.js
+++ src/fetchPictures.js
@@ -1,15 +1,32 @@
 "use strict";
+import axios from "axios";
 
 const API_KEY = '35060250-d524e63ae3659c305fff44fad';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export const fetchPictures = (name, pageNumber) => {
-     
-    return fetch(`${BASE_URL}?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${pageNumber}&per_page=4`)
-        .then(response => {
-            if(!response.ok) {
-                throw new Error (response.status);
-            }
-            return response.json();  
-        });
+export const fetchPictures = async (name, pageNumber) => {
+    try {
+        return await axios.get(`${BASE_URL}`, { //через бібліотеку axios (самостійно переводить дані із формату JSON у JS і навпаки; вміє ловити помилки)
+        params: {
+            key: API_KEY,
+            q: name,
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: true,
+            page: pageNumber,
+            per_page: 4,
+        },
+    });
+    } catch(error) {
+        throw new Error(error.message);
+    }
+    
+    // Звичайним методом (працює):
+    // return fetch(`${BASE_URL}?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${pageNumber}&per_page=4`)
+    //     .then(response => {
+    //         if(!response.ok) {
+    //             throw new Error (response.status);
+    //         }
+    //         return response.json();  
+    //     });
 }
--- src/index.js
+++ src/index.js
@@ -1,60 +1,68 @@
 "use strict";
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-import {fetchPictures} from './fetchPictures';
-import galleryCardTemplate from './gallery.hbs';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';  // бібліотека вспливаючих сповіщень
+import SimpleLightbox from "simplelightbox";                 // бібліотека для галереї
+import "simplelightbox/dist/simple-lightbox.min.css";       // бібліотеки для галереї (стилізація)
+import {fetchPictures} from './fetchPictures';             // проміс (дані з бекенду/API) 
+import galleryCardTemplate from './gallery.hbs';          // шаблонізатор (handlebar)
 
-const searchForm = document.querySelector('#search-form');
-const galleryContainer = document.querySelector('.gallery');
-const loadMoreBtn = document.querySelector('.load-more');
+const searchForm = document.querySelector('#search-form');    // <form>
+const galleryContainer = document.querySelector('.gallery'); // <div>
+const loadMoreBtn = document.querySelector('.load-more');   // <button>
 
 let searchQuery = '';
 let pageNumber = null;
 let lightbox = null;
 
-searchForm.addEventListener('submit', handleFormSubmit);
 loadMoreBtn.style.display = "none";
 
 
 // Відправка запиту/форми
+searchForm.addEventListener('submit', handleFormSubmit);
 
-function handleFormSubmit (event) {
+async function handleFormSubmit (event) {
     event.preventDefault();
     searchQuery = event.currentTarget.elements.searchQuery.value.trim();
     galleryContainer.innerHTML = "";  //Для пошуку за новим ключовим словом, очищуємо вміст галереї
     pageNumber = 1;
 
-    fetchPictures(searchQuery, pageNumber)  // виклик ф-ції-запиту до бекенду, що вертає проміс 
-        .then((data) => {
-            if(data.hits.length === 0) {
+    try {
+        const {data} = await fetchPictures(searchQuery, pageNumber);  // рівноцінний запис 2-м рядкам, що нижче
+    //fetchPictures(searchQuery, pageNumber)  // виклик ф-ції-запиту до бекенду, що вертає проміс 
+        //.then(({data}) => { // ({data}) замість (data) - у випадку застосування бібліотеки axios
+            //console.log(data); // приклад: {total: 7526, totalHits: 500, hits: Array(4)}
+            //console.log(data.hits); // масив об'єктів
+            if(data.hits.length === 0) {  // або (!data.hits.length)
                 Notify.failure('Sorry, there are no images matching your search query. Please try again.');
                 loadMoreBtn.style.display = "none";
                 return;
             } 
             Notify.success(`Hooray! We found ${data.totalHits} images.`);
             
-            renderMarkup(data.hits);
+            renderMarkup(data.hits); // Рендер розмітки
             styleGallery();  // Стилізація галереї   
 
             // Створюємо модальне вікно-галерею через бібліотеку SimpleLightbox
             lightbox = new SimpleLightbox('.gallery a', { 
                 captionSelector: 'img', 
             });  
-            
+
             loadMoreBtn.addEventListener('click', handleLoadMoreBtnClick); //  клік по кнопці "Load More"
             loadMoreBtn.style.display = "block";
 
-        }).catch(console.log);
+    } catch(err) {
+        console.log(err);
+    }
 }
 
 // Обробник події кліку на кнопку "Load More"
 
-function handleLoadMoreBtnClick () {   
+async function handleLoadMoreBtnClick () {   
     pageNumber += 1;
-    fetchPictures(searchQuery, pageNumber)
-        .then((data) => {
-            renderMarkup(data.hits);
+    try {
+        const {data} = await fetchPictures(searchQuery, pageNumber);
+    // fetchPictures(searchQuery, pageNumber)
+        // .then(({data}) => {  // ({data}) замість (data) - у випадку застосування бібліотеки axios
+            renderMarkup(data.hits);  // Рендер розмітки
             styleGallery();  // Стилізація галереї  
             lightbox.refresh(); // рефрешим бібліотеку SimpleLightbox
 
@@ -62,7 +70,9 @@ function handleLoadMoreBtnClick () {
                 Notify.failure("We're sorry, but you've reached the end of search results."); 
                 loadMoreBtn.style.display = "none";
             }
-        })
+        } catch(err) {
+            console.log(err);
+        }
 }
 
 // Рендер розмітки
@@ -85,8 +95,8 @@ function styleGallery() {
     const cardsMetaWrap = document.querySelectorAll('.info');   //контейнер підписів     
     cardsMetaWrap.forEach(cardMetaWrapItem => {
         cardMetaWrapItem.style.cssText = "display: flex; -webkit-box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 1px rgba(0, 0, 0, 0.14), 0px 2px 1px rgba(0, 0, 0, 0.2); box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 1px rgba(0, 0, 0, 0.14), 0px 2px 1px rgba(0, 0, 0, 0.2); border-radius: 0px 0px 4px 4px;";
-
     })
+
     const cardsMeta = document.querySelectorAll('.info-item');  //підписи  <p>    
     cardsMeta.forEach(cardMeta => {
         cardMeta.style.cssText = "display: flex; flex-direction: column; align-items: center; margin-left: auto; margin-right: auto;";
